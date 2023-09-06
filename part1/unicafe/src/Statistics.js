91ad186cf2830daed2930f9586314d625fbafe45
saremipegah <saremipegah@gmail.com>
2023-09-06 11:27:21 +0330

    unicafe 1.9

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,8 +1,14 @@
 const Statistics = ({good,neutral,bad}) => {
 
   const all = good+neutral+bad;
+  if(all === 0){
+    return  <p>No feedback given </p>
+   
+    
+  }
   const average = (good*(1)+neutral*(0)+bad*(-1))/all;
   const positive = (good * 100) / all;
+  
  
   
     return(
