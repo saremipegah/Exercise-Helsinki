d7f2198f5dc90f0aade969951e514e3e170f9a6c
saremipegah <saremipegah@gmail.com>
2023-09-06 10:51:16 +0330

    unicafe 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,50 +1,37 @@
 import { useState } from 'react'
 
+
 const App = () => {
   
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState([])
-  const [positive, setPositive] = useState(0)
-  
+  const all = good+neutral+bad;
+  const average = (good*(1)+neutral*(0)+bad*(-1))/all;
+  const positive = (good * 100) / all;
+ 
   
   const handleGoodtClick =() =>{
     const updatedGood = good + 1
     setGood(updatedGood)
-    setAll(updatedGood+neutral+bad)
-    const average =(good-bad) /all
-    const positive = (good * 100) / all
-    setAverage(average)
-    setPositive(positive)
   }
   const handleNeutraltClick =() =>{
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
-    setAll(updatedNeutral+good+bad)
-    const average =(good-bad) /all
-    const positive = (good * 100) / all
-    setAverage(average)
-    setPositive(positive)
-    console.log(setAverage);
+   
   }
 
   const handleBadtClick =() =>{
     const updatedBad = bad + 1
     setBad(updatedBad)
-    setAll(updatedBad+good+neutral)
-    const average =(good-bad) /all
-    const positive = (good * 100) / all 
-    setAverage(average)
-    setPositive(positive) 
+
   }
   
 
   return (
     <div>
       <h1>give feed back</h1>
-       <button onClick={handleGoodtClick}>good</button>
+      <button onClick={handleGoodtClick}>good</button>
        <button onClick={handleNeutraltClick}>neutral</button>
        <button onClick={handleBadtClick}>bad</button>
        <h2>Statistics</h2>
@@ -54,6 +41,7 @@ const App = () => {
        <p>all {all}</p>
        <p>average {average}</p>
        <p>positive {positive}%</p>
+       
     </div>
   )
 }
