129d2130eaf27fb52c15ebc83ac2856a28487af0
saremipegah <saremipegah@gmail.com>
2023-09-11 10:00:35 +0330

    2.3

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,9 +1,11 @@
 
 
 const Total = (props) => {
+
+    const all =props.parts.reduce((acc,p)=> acc + p.exercises,0)
     return ( 
         <div>
-           <strong>total of {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises +  props.parts[3].exercises} exercises</strong>
+          <strong>Total of {all} exercises</strong>
         </div>
      );
 }
